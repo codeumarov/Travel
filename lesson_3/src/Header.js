d2a7b064ce92e6f5423a19a65b427752e4ed6b67
Umarov <muhriddinumarov177@mail.com>
2023-02-18 08:54:16 +0500

    reactjs lesson_3

---
+++ lesson_3/src/Header.js
@@ -0,0 +1,26 @@
+
+
+function Header(){
+  return(
+    <>
+       <div className="Container">
+         <div className="content">
+             <ol>
+               <li><a href="">Home</a></li>
+               <li><a href="">Tours</a></li>
+               <li><a href="">Hotel</a></li>
+               <li><a href="">Gallery</a></li>
+               <li><a href="">Pages</a></li>
+               <li><a href="">Blog</a></li>
+               <li><a href="">Contact</a></li>
+               <li><a href="">Login</a></li>
+               <li><a href="">Sign Up</a></li>
+               
+             </ol>
+         </div>
+       </div>
+    </>
+  )
+}
+
+export default Header
